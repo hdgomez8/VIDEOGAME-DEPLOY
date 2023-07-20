4e038ff4cc8e7de0b8986844dcc3cda102db6894
Hector David Gomez Pertuz <hdgomez8@hotmail.com>
2023-07-20 14:12:50 -0500

    update filter

--- api/src/app.js
+++ api/src/app.js
@@ -37,7 +37,7 @@ server.use(morgan('dev'));
 // Establece cabeceras para permitir el acceso a la 
 // API desde un dominio específico y configurar los métodos HTTP permitidos.
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://videogame-deploy-seven.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://videogame-deploy.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
